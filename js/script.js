ab7a3ab0d11f98c0b8f450355ec45d8c85257211
GabrielGaudi <gabriel.rosalvo@etec.sp.gov.br>
2024-05-02 11:01:31 -0300

    adicionando arquivos

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let canvasPong = document.getElementById('canvas') //variável para manipular o elemento html com Id "canvas"
+let quadro = canvasPong.getContext('2d')
+let player1 = quadro.fillRect(100,100,50,50)
+
+
